0944511560c5453b7aa4b50d4be3ae6b28a50aa6
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-10-30 15:43:30 -0300

    Implement FromStr for ImageType and Filter
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- cataas/src/types.rs
+++ cataas/src/types.rs
@@ -1,4 +1,7 @@
+use std::str::FromStr;
+
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 use time::OffsetDateTime;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize)]
@@ -10,6 +13,24 @@ pub enum ImageType {
     Original,
 }
 
+#[derive(Debug, Error)]
+#[error("cannot parse an invalid ImageType")]
+pub struct ParseImageTypeError(());
+
+impl FromStr for ImageType {
+    type Err = ParseImageTypeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "small" | "sm" => Self::Small,
+            "medium" | "md" => Self::Medium,
+            "square" | "sq" => Self::Square,
+            "original" | "or" => Self::Original,
+            _ => return Err(ParseImageTypeError(())),
+        })
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize)]
 #[serde(rename = "lowercase")]
 pub enum Filter {
@@ -19,6 +40,26 @@ pub enum Filter {
     Negative,
     Paint,
     Pixel,
+}
+
+#[derive(Debug, Error)]
+#[error("cannot parse an invalid Filter")]
+pub struct ParseFilterError(());
+
+impl FromStr for Filter {
+    type Err = ParseFilterError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "blur" => Self::Blur,
+            "mono" => Self::Mono,
+            "sepia" => Self::Sepia,
+            "negative" => Self::Negative,
+            "paint" => Self::Paint,
+            "pixel" => Self::Pixel,
+            _ => return Err(ParseFilterError(())),
+        })
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize)]
