f9520a7ac8ad3562737acd767724c68f57ba9a8a
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-10-30 15:44:54 -0300

    cataacli: Implement tags command
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- cataacli/src/main.rs
+++ cataacli/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
+#![warn(clippy::pedantic, clippy::cargo)]
+
+use anyhow::Result;
+use cataas::Client;
+use clap::Parser;
+
+const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Parser)]
+enum Command {
+    Get,
+    Say,
+    /// List available tags
+    Tags,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let mut user_agent = USER_AGENT.to_owned();
+    user_agent.push(' ');
+    user_agent.push_str(cataas::USER_AGENT);
+    let client = Client::builder().user_agent(user_agent).build();
+
+    match args.command {
+        Command::Get | Command::Say => {
+            todo!()
+        }
+        Command::Tags => client
+            .tags()
+            .await?
+            .iter()
+            .filter(|tag| !tag.is_empty())
+            .for_each(|tag| println!("{}", tag)),
+    }
+
     println!("Hello, world!");
+
+    Ok(())
 }
--- cataas/src/lib.rs
+++ cataas/src/lib.rs
@@ -2,7 +2,7 @@
 #![allow(clippy::missing_errors_doc)]
 
 mod client;
-
-pub use client::*;
 pub mod methods;
 pub mod types;
+
+pub use client::*;
